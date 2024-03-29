bc15a21970611549394de874d44c7674c9bfc22a
Moemen Ouled Ali <ouledali.moemen@esprit.tn>
2024-03-29 13:36:59 +0100

    Revert "added and installed Template"
    This reverts commit 5f4178eaab38b55741c6ffb92fec14f6bb620137.

--- atsreact/Template/full-version/src/hooks/useConfig.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { ConfigContext } from 'contexts/ConfigContext';
-
-// ==============================|| HOOKS - CONFIG  ||============================== //
-
-const useConfig = () => useContext(ConfigContext);
-
-export default useConfig;
