7d39ea56b8dd5545d0e24a711484068528e58a37
Moemen Ouled Ali <ouledali.moemen@esprit.tn>
2024-04-11 23:39:45 +0000

    Update JobDetailsPage.js

--- atsreact/src/Components/JobDetailsPage.js
+++ atsreact/src/Components/JobDetailsPage.js
@@ -21,6 +21,7 @@ const JobDetailsPage = () => {
     useEffect(() => {
         const fetchJobDetails = async () => {
             try {
+              console.log(`Making API request to: http://localhost:5000/api/jobs/${id}`);
                 const response = await axios.get(`http://localhost:5000/api/jobs/${id}`);
                 setJobDetails(response.data);
             } catch (error) {
@@ -55,103 +56,104 @@ const JobDetailsPage = () => {
     return (
         <div>
             <Typography variant="h4" style={{ marginBottom: 20 }}>Apply for {jobDetails.title || 'the position'}</Typography>
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    label="Full Name"
-                    name="fullName"
-                    value={application.fullName}
-                    fullWidth
-                    margin="normal"
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-                <TextField
-                    label="Age"
-                    name="age"
-                    value={application.age}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    type="number"
-                />
-                <FormControl fullWidth margin="normal">
-                    <InputLabel>Highest Level of Education</InputLabel>
-                    <Select
-                        value={application.educationLevel}
-                        onChange={handleChange}
-                        input={<OutlinedInput label="Highest Level of Education" name="educationLevel" />}
-                    >
-                        <MenuItem value="Baccalaureate">Baccalaureate</MenuItem>
-                        <MenuItem value="License">License</MenuItem>
-                        <MenuItem value="Engineering">Engineering</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl fullWidth margin="normal">
-                    <InputLabel>Years of Professional Experience</InputLabel>
-                    <Select
-                        value={application.experience}
-                        onChange={handleChange}
-                        input={<OutlinedInput label="Years of Professional Experience" name="experience" />}
-                    >
-                        <MenuItem value="0">0</MenuItem>
-                        <MenuItem value="1-3">1-3</MenuItem>
-                        <MenuItem value="4-6">4-6</MenuItem>
-                        <MenuItem value="7+">7+</MenuItem>
-                    </Select>
-                </FormControl>
-                <TextField
-                    label="University"
-                    name="university"
-                    value={application.university}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    label="Email Address"
-                    name="email"
-                    value={application.email}
-                    fullWidth
-                    margin="normal"
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-                <TextField
-                    label="Phone Number"
-                    name="phoneNumber"
-                    value={application.phoneNumber}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    label="Motivation Letter"
-                    name="motivationLetter"
-                    value={application.motivationLetter}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    multiline
-                    rows={4}
-                />
-                <Button
-                    variant="contained"
-                    component="label"
-                    style={{ marginTop: 20, marginBottom: 20 }}
-                >
-                    Upload Resume (PDF)
-                    <input
-                        type="file"
-                        name="resume"
-                        hidden
-                        accept=".pdf"
-                        onChange={handleChange}
-                    />
-                </Button>
-                <Button type="submit" variant="contained" color="primary" fullWidth>Submit Application</Button>
-            </form>
+<form onSubmit={handleSubmit}>
+    <TextField
+        label="Full Name"
+        name="fullName"
+        value={application.fullName}
+        fullWidth
+        margin="normal"
+        InputProps={{
+            readOnly: true,
+        }}
+    />
+    <TextField
+        label="Age"
+        name="age"
+        value={application.age}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+        type="number"
+    />
+    <FormControl fullWidth margin="normal">
+        <InputLabel>Highest Level of Education</InputLabel>
+        <Select
+            value={application.educationLevel}
+            onChange={handleChange}
+            input={<OutlinedInput label="Highest Level of Education" name="educationLevel" />}
+        >
+            <MenuItem value="Baccalaureate">Baccalaureate</MenuItem>
+            <MenuItem value="License">License</MenuItem>
+            <MenuItem value="Engineering">Engineering</MenuItem>
+        </Select>
+    </FormControl>
+    <FormControl fullWidth margin="normal">
+        <InputLabel>Years of Professional Experience</InputLabel>
+        <Select
+            value={application.experience}
+            onChange={handleChange}
+            input={<OutlinedInput label="Years of Professional Experience" name="experience" />}
+        >
+            <MenuItem value="0">0</MenuItem>
+            <MenuItem value="1-3">1-3</MenuItem>
+            <MenuItem value="4-6">4-6</MenuItem>
+            <MenuItem value="7+">7+</MenuItem>
+        </Select>
+    </FormControl>
+    <TextField
+        label="University"
+        name="university"
+        value={application.university}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+    />
+    <TextField
+        label="Email Address"
+        name="email"
+        value={application.email}
+        fullWidth
+        margin="normal"
+        InputProps={{
+            readOnly: true,
+        }}
+    />
+    <TextField
+        label="Phone Number"
+        name="phoneNumber"
+        value={application.phoneNumber}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+    />
+    <TextField
+        label="Motivation Letter"
+        name="motivationLetter"
+        value={application.motivationLetter}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+        multiline
+        rows={4}
+    />
+    <Button
+        variant="contained"
+        component="label"
+        style={{ marginTop: 20, marginBottom: 20 }}
+    >
+        Upload Resume (PDF)
+        <input
+            type="file"
+            name="resume"
+            hidden
+            accept=".pdf"
+            onChange={handleChange}
+        />
+    </Button>
+    <Button type="submit" variant="contained" color="primary" fullWidth>Submit Application</Button>
+</form>
+
         </div>
     );
 };
